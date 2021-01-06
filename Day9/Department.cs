0845bef413a575fc50d01de3255266eb3d5b7093
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-01-06 19:45:07 +0200

    Initial commit

---
+++ Day9/Department.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9
+{
+    class Department
+    {
+        public int DeptID { get; set; }
+        public string DeptName { get; set; }
+
+        List<Employee> Staff;
+        public void AddStaff(Employee E)
+        {
+            throw new NotImplementedException();
+            ///Try Register for EmployeeLayOff Event Here
+        }
+        ///CallBackMethod
+        public void RemoveStaff(object sender,
+       EmployeeLayOffEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Day9/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9
+{
+    class Employee
+    {
+        public event
+       EventHandler<EmployeeLayOffEventArgs> EmployeeLayOff;
+        protected virtual void OnEmployeeLayOff
+       (EmployeeLayOffEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        public int EmployeeID { get; set; }
+        public DateTime BirthDate
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+        public int VacationStock
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+        public bool RequestVacation(DateTime From, DateTime To)
+        {
+            throw new NotImplementedException();
+        }
+        public void EndOfYearOperation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public enum LayOffCause
+    { ///Implement it YourSelf 
+    }
+    public class EmployeeLayOffEventArgs
+    {
+        public LayOffCause Cause { get; set; }
+    }
+
+    class SalesPerson : Employee
+    {
+        public int AchievedTarget { get; set; }
+        public bool CheckTarget(int Quota)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class BoardMember : Employee
+    {
+        public void Resign()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
