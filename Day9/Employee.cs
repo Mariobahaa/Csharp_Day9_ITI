045b1932bca7228cc8b6c13df6bec3a70cc46cbf
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-01-06 21:22:29 +0200

    Implemented Employee V2.0 >Check Conditions in onEmployeeLayOff Method

--- Day9/Employee.cs
+++ Day9/Employee.cs
@@ -11,8 +11,9 @@ namespace Day9
         protected virtual void OnEmployeeLayOff
        (EmployeeLayOffEventArgs e)
         {
-            //if(VacationStock < 0 || (DateTime.Now - BirthDate).Days > ((365*60)+15))
-            EmployeeLayOff.Invoke(this, e);
+            DateTime retirement = BirthDate.AddYears(60);
+            if (DateTime.Now.CompareTo(retirement) > 0 || VacationStock < 0)
+                EmployeeLayOff.Invoke(this, e);
         }
         public int EmployeeID { get; set; }
         public DateTime BirthDate
@@ -28,24 +29,14 @@ namespace Day9
         public bool RequestVacation(DateTime From, DateTime To)
         {
             int reqDays = (To - From).Days;
-            if(reqDays <= VacationStock)
-            {
-                VacationStock -= reqDays;
-                return true;
-            }
-            else
-            {
-                OnEmployeeLayOff(new EmployeeLayOffEventArgs() { Cause = LayOffCause.fired });
-                return false;
-            }
+            VacationStock -= reqDays;
+            OnEmployeeLayOff(new EmployeeLayOffEventArgs() { Cause = LayOffCause.fired });
+
+            return VacationStock >=0;   
         }
         public void EndOfYearOperation()
-        {
-            DateTime retirement = BirthDate.AddYears(60);
-            if(DateTime.Now.CompareTo(retirement) > 0)
-            {
-                OnEmployeeLayOff(new EmployeeLayOffEventArgs() { Cause = LayOffCause.retired });
-            }
+        {       
+              OnEmployeeLayOff(new EmployeeLayOffEventArgs() { Cause = LayOffCause.retired });  
         }
         
     }
